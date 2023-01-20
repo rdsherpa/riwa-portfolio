21b57856a75c4e50fd2fb6dcd6e137dd666751fd
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-01-19 22:18:54 -0800

    initial set-up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <>
+      <About />
+    </>
+  );
+}
+
+export default App;
