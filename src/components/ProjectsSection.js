c3aec6f216f3b19fa4eae5e8cb3f8d53a7a5b736
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2023-01-28 09:25:39 -0800

    worked on the project section

---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,66 @@
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import projects from "../assets/data/projects";
+import ProjectItem from "./ProjectItem";
+import styled from "styled-components";
+import "swiper/swiper-bundle.min.css";
+
+SwiperCore.use([Navigation]);
+
+const ProjectSectionStyle = styled.div`
+  padding: 10rem 0;
+  .projects__allItems {
+    display: flex;
+    gap: 3rem;
+    margin-top: 5rem;
+  }
+  .swiper-container {
+    padding-top: 8rem;
+    max-width: 100%;
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    position: absolute;
+    height: 50px;
+    width: 50px;
+    background-color: var(--deep-dark);
+    z-index: 10;
+    right: 60px;
+    left: auto;
+    top: 0;
+    transform: translateY(50%);
+    color: var(--gray-1);
+    border-radius: 8px;
+  }
+  .swiper-button-next {
+    right: 0;
+  }
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 2rem;
+  }
+`;
+
+export default function ProjectsSection() {
+  return (
+    <ProjectSectionStyle>
+      <div className="container">
+        <SectionTitle heading="Projects" subheading="some of my recent works" />
+        <div className="Projects__allItems">
+          <Swiper spaceBetween={30} slidesPerView={2} navigation>
+            {projects.map((project, index) => {
+              if (index >= 5) return;
+              return (
+                <SwiperSlide key={project.id}>
+                  <ProjectItem />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </ProjectSectionStyle>
+  );
+}
